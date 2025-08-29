4d8f554f6a2f1fad5e64e9cd9de7be220c8fb5dd
Aquib younis <aquibyounis1@gmail.com>
2025-08-29 11:17:32 +0530

    get and post methods updated in routing

--- routes/bfhl.js
+++ routes/bfhl.js
@@ -4,6 +4,10 @@ const { handlePost } = require("../controllers/bfhlController");
 
 
 router.post("/", handlePost);
+router.get("/",(req,res)=> {
+    console.log("BFHL route is working");
+    res.json({ message: "✅ BFHL route is working" });
+});
 
 
 module.exports = router;
